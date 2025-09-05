769c62a9bbcc46fbe104f807c7e89c20f31351e8
Mike Nolan <tesses@tesses.net>
2025-09-05 12:43:55 -0500

    Fix builds

--- Packaging/Linux/push-ubuntu-jammy.sh
+++ Packaging/Linux/push-ubuntu-jammy.sh
@@ -4,7 +4,7 @@ upload() {
     curl --user tesses50:$GITEA_AUTH -X DELETE \
     https://git.tesseslanguage.com/api/packages/tesses50/debian/pool/jammy/main/crosslang/$DEB_VERSION/$1
     curl --user tesses50:$GITEA_AUTH \
-    --upload-file build/jammy/crosslang/$DEB_VERSION\_$1\.deb \
+    --upload-file build/jammy/crosslang_$DEB_VERSION\_$1\.deb \
     https://git.tesseslanguage.com/api/packages/tesses50/debian/pool/jammy/main/upload
 }
 upload amd64
--- Packaging/Linux/push-ubuntu-plucky.sh
+++ Packaging/Linux/push-ubuntu-plucky.sh
@@ -4,7 +4,7 @@ upload() {
     curl --user tesses50:$GITEA_AUTH -X DELETE \
     https://git.tesseslanguage.com/api/packages/tesses50/debian/pool/plucky/main/crosslang/$DEB_VERSION/$1
     curl --user tesses50:$GITEA_AUTH \
-    --upload-file build/plucky/crosslang/$DEB_VERSION\_$1\.deb \
+    --upload-file build/plucky/crosslang_$DEB_VERSION\_$1\.deb \
     https://git.tesseslanguage.com/api/packages/tesses50/debian/pool/plucky/main/upload
 }
 upload amd64
