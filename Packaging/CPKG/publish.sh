a38451ce6955136dc8ce6036c3a8cfe5e6d24409
Mike Nolan <tesses@tesses.net>
2025-09-26 16:40:06 -0500

    Add runtime packages to CPKG

--- Packaging/CPKG/publish.sh
+++ Packaging/CPKG/publish.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find publish -name "*.crvm" -exec upload-package --token="$CPKG_KEY" --host="https://cpkg.tesseslanguage.com/" "{}" \;
+find publish -name "*.crvm" -exec crosslang upload-package --token="$CPKG_KEY" --host="https://cpkg.tesseslanguage.com/" "{}" \;
