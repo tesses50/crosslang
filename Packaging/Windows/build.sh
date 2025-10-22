e1740131c490ea79f4d297683bd362170af08f80
Mike Nolan <tesses@tesses.net>
2025-10-22 15:03:17 -0500

    Fix scripts

--- Packaging/Windows/build.sh
+++ Packaging/Windows/build.sh
@@ -4,13 +4,13 @@ cd build-x86
 make -j`nproc`
 cpack -G NSIS
 cpack -G ZIP
-cp TessesCrossLang-*-win32.exe ../../../artifacts/crosslang-win32.exe
-cp TessesCrossLang-*-win32.zip ../../../artifacts/crosslang-win32.zip
+mv TessesCrossLang-*-win32.exe ../../../artifacts/crosslang-win32.exe
+mv TessesCrossLang-*-win32.zip ../../../artifacts/crosslang-win32.zip
 cd ..
 cmake -S ../.. -B build-x64 -DCMAKE_TOOLCHAIN_FILE=`pwd`/WindowsToolchains/x64.cmake -DTESSESFRAMEWORK_FETCHCONTENT=ON -DTESSESFRAMEWORK_ENABLE_APPS=OFF -DTESSESFRAMEWORK_ENABLE_EXAMPLES=OFF -DTESSESFRAMEWORK_ENABLE_STATIC=ON -DTESSESFRAMEWORK_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=tmp-x64
 cd build-x64
 make -j`nproc`
 cpack -G NSIS
 cpack -G ZIP
-cp TessesCrossLang-*-win64.exe ../../../artifacts/crosslang-win64.exe
-cp TessesCrossLang-*-win64.zip ../../../artifacts/crosslang-win64.zip
+mv TessesCrossLang-*-win64.exe ../../../artifacts/crosslang-win64.exe
+mv TessesCrossLang-*-win64.zip ../../../artifacts/crosslang-win64.zip
